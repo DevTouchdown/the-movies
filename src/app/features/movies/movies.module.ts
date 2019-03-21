773b7fdf4687d65c1bb49587837527dee011e104
DevTouchdown <devtouchdown@outlook.com>
2019-03-21 19:15:31 +0100

    Deleted faulty import from MoviesModule.

--- src/app/features/movies/movies.module.ts
+++ src/app/features/movies/movies.module.ts
@@ -4,12 +4,10 @@ import { CommonModule } from '@angular/common';
 import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesComponent } from './movies/movies.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    MoviesComponent,
-    DetailsComponent
+    MoviesComponent
   ],
   imports: [
     CommonModule,
